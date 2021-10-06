9dce2231d95a56bf5255f5737034c00bb31b2834
Mark Gauffin <markgauffin@gmail.com>
2021-10-06 05:52:20 -0500

    restructure filetree, add gitignore, changed secret key and stored it outside of repository

--- therapy/settings.py
+++ therapy/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 
+# - New - #
+from decouple import config
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-3x^q!e2)$*z9ubvo1c(6-^f+2ql1v8%j47_3q!(6w9ymy-vqd^'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
