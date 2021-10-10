ee8a7741d3099f5a05b89e9fb9c8915764016333
Mark Gauffin <markgauffin@gmail.com>
2021-10-10 03:24:03 -0500

    create model

--- biblehelp/models.py
+++ biblehelp/models.py
@@ -1,3 +1,11 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 # Create your models here.
+
+class User(AbstractUser):
+    pass
+
+class Goal:
+    goal = models.CharField(max_length=100)
+    setter = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
