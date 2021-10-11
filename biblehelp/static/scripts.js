f5cdb7c516928520c827511a7d5aab685da83394
Mark Gauffin <markgauffin@gmail.com>
2021-10-11 04:49:14 -0500

    testing js form validation

---
+++ biblehelp/static/scripts.js
@@ -0,0 +1,8 @@
+// const form = document.querySelector('form');
+// console.log(form);
+
+// const btn = document.querySelector('.btn-submit');
+// btn.addEventListener('click', (e) => {
+//     e.preventDefault();
+//     console.log(btn);
+// });
