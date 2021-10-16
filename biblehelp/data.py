9aa6c51ec35fdeaed86ed13f2572684a31d246fd
Mark Gauffin <markgauffin@gmail.com>
2021-10-16 05:24:42 -0500

    change field names

--- biblehelp/data.py
+++ biblehelp/data.py
@@ -1,53 +1,73 @@
-stress = [
+initial_data = [
     {
-        'scripture': 'Matthew 6:34',
-        'text': 'So never be anxious about the next day, for the next day will have its own anxieties. Each day has enough of its own troubles.',
-        'advice': 'Take one day at a time'
+        'id': '1',
+        'topic': 'Stress',
+        'cite': 'Matthew 6:34',
+        'scripture_text': 'So never be anxious about the next day, for the next day will have its own anxieties. Each day has enough of its own troubles.',
+        'goal': 'Take one day at a time'
     },
     {
-        'scripture': 'James 3:17', 
-        'text': 'But the wisdom from above is first of all pure, then peaceable, reasonable, ready to obey, full of mercy and good fruits, impartial, not hypocritical.',
-        'advice': 'Set reasonable standards'
+        'id': '2',
+        'topic': 'Stress',
+        'cite': 'James 3:17', 
+        'scripture_text': 'But the wisdom from above is first of all pure, then peaceable, reasonable, ready to obey, full of mercy and good fruits, impartial, not hypocritical.',
+        'goal': 'Set reasonable standards'
     },
     {
-        'scripture': 'Proverbs 17:27',
-        'text': 'A man of knowledge restrains his words, And a discerning man will remain calm.',
-        'advice': 'Get to know what stresses you'
+        'id': '3',
+        'topic': 'Stress',
+        'cite': 'Proverbs 17:27',
+        'scripture_text': 'A man of knowledge restrains his words, And a discerning man will remain calm.',
+        'goal': 'Get to know what stresses you'
     },
     {
-        'scripture': '1 Corinthians 14:40',
-        'text': 'But let all things take place decently and by arrangement.',
-        'advice': 'Try to be orderly'
+        'id': '4',
+        'topic': 'Stress',
+        'cite': '1 Corinthians 14:40',
+        'scripture_text': 'But let all things take place decently and by arrangement.',
+        'goal': 'Try to be orderly'
     },
     {
-        'scripture': 'Ecclesiasties 4:6',
-        'text': 'Better is a handful of rest than two handfuls of hard work and chasing after the wind.',
-        'advice': 'Pursue a balanced lifestyle'
+        'id': '5',
+        'topic': 'Stress',
+        'cite': 'Ecclesiasties 4:6',
+        'scripture_text': 'Better is a handful of rest than two handfuls of hard work and chasing after the wind.',
+        'goal': 'Pursue a balanced lifestyle'
     },
     {
-        'scripture': '1 Timothy 4:8',
-        'text': 'For physical training is beneficial for a little, but godly devotion is beneficial for all things, as it holds promise of the life now and the life that is to come.',
-        'advice': 'Take care of your health'
+        'id': '6',
+        'topic': 'Stress',
+        'cite': '1 Timothy 4:8',
+        'scripture_text': 'For physical training is beneficial for a little, but godly devotion is beneficial for all things, as it holds promise of the life now and the life that is to come.',
+        'goal': 'Take care of your health'
     },
     {
-        'scripture': 'Proverbs 11:17',
-        'text': 'A kind man benefits himself, But the cruel person brings trouble on himself.',
-        'advice': 'Be kind to yourself and others'
+        'id': '7',
+        'topic': 'Stress',
+        'cite': 'Proverbs 11:17',
+        'scripture_text': 'A kind man benefits himself, But the cruel person brings trouble on himself.',
+        'goal': 'Be kind to yourself and others'
     },
     {
-        'scripture': 'Phillipians 1:10',
-        'text': 'That you may make sure of the more important things, so that you may be flawless and not stumbling others up to the day of Christ',
-        'advice': 'Set priorities'
+        'id': '8',
+        'topic': 'Stress',
+        'cite': 'Phillipians 1:10',
+        'scripture_text': 'That you may make sure of the more important things, so that you may be flawless and not stumbling others up to the day of Christ',
+        'goal': 'Set priorities'
     },
     {
-        'scripture': 'Proverbs 12:25',
-        'text': "Anxiety in a man's heart weighs it down, But a good word cheers it up.",
-        'advice': 'Get support'
+        'id': '9',
+        'topic': 'Stress',
+        'cite': 'Proverbs 12:25',
+        'scripture_text': "Anxiety in a man's heart weighs it down, But a good word cheers it up.",
+        'goal': 'Get support'
     },
     {
-        'scripture': 'Matthew 5:3',
-        'text': 'Happy are those conscious of their spiritual need, since the Kingdom of the heavens belongs to them.',
-        'advice': 'Care for your spiritual need'
+        'id': '10',
+        'topic': 'Stress',
+        'cite': 'Matthew 5:3',
+        'scripture_text': 'Happy are those conscious of their spiritual need, since the Kingdom of the heavens belongs to them.',
+        'goal': 'Care for your spiritual need'
     }
 
 ]
