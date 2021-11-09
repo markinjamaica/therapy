ee4a4d94653863babeff7581dfae5aca13daa308
Mark Gauffin <markgauffin@gmail.com>
2021-11-09 06:34:51 -0500

    add localhost to allowed hosts

--- therapy/settings.py
+++ therapy/settings.py
@@ -30,7 +30,7 @@ BIBLE_API_KEY = config('BIBLE_API_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['thinkingbiblically.pythonanywhere.com']
+ALLOWED_HOSTS = ['thinkingbiblically.pythonanywhere.com', '127.0.0.1']
 
 
 # Application definition
